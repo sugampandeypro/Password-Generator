5bd9d05c6263078f96f481065f666287e200dcbf
Sugam Pandey <ktw72@txstate.edu>
2026-02-01 22:02:48 -0600

    Fix password generation loop indices and output

--- password.py
+++ password.py
@@ -3,18 +3,19 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 password_list=[]
-for i in range(1,nr_letters+1):
+for i in range(0,nr_letters):
       password_list.append(random.choice(letters))
 
-for i in range(1,nr_symbols+1):
+for i in range(0,nr_symbols):
     password_list.append(random.choice(symbols))
 
-for i in range(1,nr_numbers+1):
+for i in range(0,nr_numbers):
     password_list.append(random.choice(numbers))
 
 random.shuffle(password_list)
@@ -22,4 +23,4 @@ random.shuffle(password_list)
 password=''
 for i in password_list:
     password+= i
-print(f"Your password is {password}")    
+print(f"Your password is: {password}")    
